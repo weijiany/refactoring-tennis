c4328e63878abcfb6886e7188a8463780909cfd0
weijian <weijian.yang@thoughtworks.com>
2020-03-03 22:52:45 +0800

    extract scoreOfDeuce when compare equal

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -10,6 +10,7 @@ public class TennisGame2 implements TennisGame {
     public String P2res = "";
     private String player1Name;
     private String player2Name;
+    private final String[] SCORE_DESCRIPTIONS = new String[] {"Love", "Fifteen", "Thirty", "Forty"};
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -17,18 +18,7 @@ public class TennisGame2 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        if (P1point == P2point && P1point < 4) {
-            if (P1point == 0)
-                score = "Love";
-            if (P1point == 1)
-                score = "Fifteen";
-            if (P1point == 2)
-                score = "Thirty";
-            score += "-All";
-        }
-        if (P1point == P2point && P1point >= 3)
-            score = "Deuce";
+        String score = scoreOfDeuce();
 
         if (P1point > 0 && P2point == 0) {
             if (P1point == 1)
@@ -91,6 +81,16 @@ public class TennisGame2 implements TennisGame {
             score = "Win for player2";
         }
         return score;
+    }
+
+    private String scoreOfDeuce() {
+        if (P1point == P2point) {
+            if (P1point < 3)
+                return SCORE_DESCRIPTIONS[P1point] + "-All";
+            return "Deuce";
+        }
+
+        return "";
     }
 
     public void P1Score() {
