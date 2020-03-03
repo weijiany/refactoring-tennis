d7cfc251079f52af8b85ba2bd78b6ac910ca79dc
weijian <weijian.yang@thoughtworks.com>
2020-03-03 22:57:52 +0800

    extract scoreForWin when either of them >= 4 and subtract value >= 2

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -20,19 +20,28 @@ public class TennisGame2 implements TennisGame {
 
         score = scoreOfNotEqual(score);
 
-        if (P1point > P2point && P2point >= 3) {
-            score = "Advantage player1";
-        }
+        score = scoreOfAdvantage(score);
 
-        if (P2point > P1point && P1point >= 3) {
-            score = "Advantage player2";
-        }
+        return scoreForWin(score);
+    }
 
+    private String scoreForWin(String score) {
         if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
             score = "Win for player1";
         }
         if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
             score = "Win for player2";
+        }
+        return score;
+    }
+
+    private String scoreOfAdvantage(String score) {
+        if (P1point > P2point && P2point >= 3) {
+            return "Advantage " + player1Name;
+        }
+
+        if (P2point > P1point && P1point >= 3) {
+            return "Advantage " + player2Name;
         }
         return score;
     }
@@ -50,7 +59,6 @@ public class TennisGame2 implements TennisGame {
                 return SCORE_DESCRIPTIONS[P1point] + "-All";
             return "Deuce";
         }
-
         return "";
     }
 
