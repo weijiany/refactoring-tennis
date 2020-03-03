f66c21bcdd75dc043d121ce219455d5a6ba7fe19
weijian <weijian.yang@thoughtworks.com>
2020-03-03 22:54:31 +0800

    extract scoreOfNotEqual when compare different and both of scores are less than 4

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -6,8 +6,6 @@ public class TennisGame2 implements TennisGame {
     public int P1point = 0;
     public int P2point = 0;
 
-    public String P1res = "";
-    public String P2res = "";
     private String player1Name;
     private String player2Name;
     private final String[] SCORE_DESCRIPTIONS = new String[] {"Love", "Fifteen", "Thirty", "Forty"};
@@ -20,51 +18,7 @@ public class TennisGame2 implements TennisGame {
     public String getScore() {
         String score = scoreOfDeuce();
 
-        if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-
-        if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            score = P1res + "-" + P2res;
-        }
+        score = scoreOfNotEqual(score);
 
         if (P1point > P2point && P2point >= 3) {
             score = "Advantage player1";
@@ -79,6 +33,13 @@ public class TennisGame2 implements TennisGame {
         }
         if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
             score = "Win for player2";
+        }
+        return score;
+    }
+
+    private String scoreOfNotEqual(String score) {
+        if (P1point < 4 && P2point < 4 && P1point != P2point) {
+            return SCORE_DESCRIPTIONS[P1point] + "-" + SCORE_DESCRIPTIONS[P2point];
         }
         return score;
     }
