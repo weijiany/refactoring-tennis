06170ffe9ea8b9e45d00d75a4d3579973d918e47
weijian <weijian.yang@thoughtworks.com>
2020-03-03 20:58:02 +0800

    extract SCORE_DESCRIPTIONS be used to save description of score.
    aaa

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -8,6 +8,7 @@ public class TennisGame1 implements TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private final String[] SCORE_DESCRIPTIONS = new String[] {"Love", "Fifteen", "Thirty", "Forty"};
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -27,48 +28,28 @@ public class TennisGame1 implements TennisGame {
         if (isAdvantage())
             return scoreOfAdvantage();
 
-        return scoreOfDescription(m_score1) + '-' + scoreOfDescription(m_score2);
-    }
-
-    private String scoreOfDescription(int tempScore) {
-        switch (tempScore) {
-            case 0:
-                return "Love";
-            case 1:
-                return "Fifteen";
-            case 2:
-                return "Thirty";
-            case 3:
-                return "Forty";
-            default:
-                throw new RuntimeException();
-        }
+        return SCORE_DESCRIPTIONS[m_score1] + '-' + SCORE_DESCRIPTIONS[m_score2];
     }
 
     private String scoreOfAdvantage() {
         int minusResult = m_score1 - m_score2;
-        if (minusResult == 1) return "Advantage player1";
-        else if (minusResult == -1) return "Advantage player2";
-        else if (minusResult >= 2) return "Win for player1";
-        else return "Win for player2";
+        if (minusResult == 1)
+            return "Advantage player1";
+        if (minusResult == -1)
+            return "Advantage player2";
+        if (minusResult >= 2)
+            return "Win for player1";
+        return "Win for player2";
+    }
+
+    private String scoreOfDeuce() {
+        if (m_score1 < 3)
+            return SCORE_DESCRIPTIONS[m_score1] + "-All";
+        return "Deuce";
     }
 
     private boolean isAdvantage() {
         return m_score1 >= 4 || m_score2 >= 4;
-    }
-
-    private String scoreOfDeuce() {
-        switch (m_score1) {
-            case 0:
-                return "Love-All";
-            case 1:
-                return "Fifteen-All";
-            case 2:
-                return "Thirty-All";
-            default:
-                return "Deuce";
-
-        }
     }
 
     private boolean isDeuce(int m_score1, int m_score2) {
