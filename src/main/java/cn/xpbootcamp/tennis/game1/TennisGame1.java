bbf3aa2523bcd4a20a78d59074772c236f6abe3f
weijian <weijian.yang@thoughtworks.com>
2020-03-03 20:48:42 +0800

    use extract method way to refactor first code.

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -22,49 +22,56 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        int tempScore;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
-                case 0:
-                    return "Love-All";
-                case 1:
-                    return "Fifteen-All";
-                case 2:
-                    return "Thirty-All";
-                default:
-                    return "Deuce";
+        if (isDeuce(m_score1, m_score2))
+            return scoreOfDeuce();
+        if (isAdvantage())
+            return scoreOfAdvantage();
 
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) return "Advantage player1";
-            else if (minusResult == -1) return "Advantage player2";
-            else if (minusResult >= 2) return "Win for player1";
-            else return "Win for player2";
-        } else {
-            StringBuilder score = new StringBuilder();
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
-                    score.append("-");
-                    tempScore = m_score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
-            }
-            return score.toString();
+        return scoreOfDescription(m_score1) + '-' + scoreOfDescription(m_score2);
+    }
+
+    private String scoreOfDescription(int tempScore) {
+        switch (tempScore) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                throw new RuntimeException();
         }
+    }
+
+    private String scoreOfAdvantage() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) return "Advantage player1";
+        else if (minusResult == -1) return "Advantage player2";
+        else if (minusResult >= 2) return "Win for player1";
+        else return "Win for player2";
+    }
+
+    private boolean isAdvantage() {
+        return m_score1 >= 4 || m_score2 >= 4;
+    }
+
+    private String scoreOfDeuce() {
+        switch (m_score1) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
+
+        }
+    }
+
+    private boolean isDeuce(int m_score1, int m_score2) {
+        return m_score1 == m_score2;
     }
 }
