eadb891b8bff31684e04f7b5aed2e06e17b92a6f
weijian <weijian.yang@thoughtworks.com>
2020-03-06 20:10:53 +0800

    rename and add statement for if

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -4,8 +4,8 @@ import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int mScore1 = 0;
+    private int mScore2 = 0;
     private String player1Name;
     private String player2Name;
     private final String[] SCORE_DESCRIPTIONS = new String[] {"Love", "Fifteen", "Thirty", "Forty"};
@@ -16,40 +16,47 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (player1Name.equals(playerName))
-            m_score1 += 1;
-        else if (player2Name.equals(playerName))
-            m_score2 += 1;
+        if (player1Name.equals(playerName)) {
+            mScore1 += 1;
+        } else if (player2Name.equals(playerName)) {
+            mScore2 += 1;
+        }
     }
 
     public String getScore() {
-        if (isDeuce(m_score1, m_score2))
+        if (isDeuce(mScore1, mScore2)) {
             return scoreOfDeuce();
-        if (isAdvantage())
+        }
+        if (isAdvantage()) {
             return scoreOfAdvantage();
+        }
 
-        return SCORE_DESCRIPTIONS[m_score1] + '-' + SCORE_DESCRIPTIONS[m_score2];
+        return SCORE_DESCRIPTIONS[mScore1] + '-' + SCORE_DESCRIPTIONS[mScore2];
     }
 
     private String scoreOfAdvantage() {
-        int minusResult = m_score1 - m_score2;
-        if (minusResult == 1)
+        int minusResult = mScore1 - mScore2;
+        if (minusResult == 1) {
             return "Advantage " + player1Name;
-        if (minusResult == -1)
+        }
+        if (minusResult == -1) {
             return "Advantage " + player2Name;
-        if (minusResult >= 2)
+        }
+        if (minusResult >= 2) {
             return "Win for " + player1Name;
+        }
         return "Win for " + player2Name;
     }
 
     private String scoreOfDeuce() {
-        if (m_score1 < 3)
-            return SCORE_DESCRIPTIONS[m_score1] + "-All";
+        if (mScore1 < 3) {
+            return SCORE_DESCRIPTIONS[mScore1] + "-All";
+        }
         return "Deuce";
     }
 
     private boolean isAdvantage() {
-        return m_score1 >= 4 || m_score2 >= 4;
+        return mScore1 >= 4 || mScore2 >= 4;
     }
 
     private boolean isDeuce(int m_score1, int m_score2) {
